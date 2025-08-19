6c7f1a9af001a57c3fb5464eed83629331f9c748
IsaacYNWA <isaacynwa03@gmail.com>
2025-08-19 16:08:57 -0300

    Add files via upload

---
+++ minimax_lab.py
@@ -0,0 +1,111 @@
+import random # Para poder elegir movimientos al azar
+import copy 
+
+
+#Varibles constantes 
+GATO = 'G'
+RATON = 'R'
+VACIO = ''
+
+def crear_tablero(ancho, alto):
+    tablero = []
+
+    for _ in range(alto):
+        fila_actual = []
+
+        for _ in range(ancho):
+            fila_actual.append(VACIO)
+        
+        tablero.append(fila_actual)
+
+    return tablero
+
+def colocar_personajes(tablero, pos_gato, pos_raton):
+    fila_gato, col_gato = pos_gato
+    fila_raton, col_raton = pos_raton
+
+    tablero[fila_gato][col_gato] = GATO
+    tablero[fila_raton][col_raton] = RATON
+
+def imprimir_tablero(tablero):
+    for fila in tablero:
+        print(' | '.join(fila))
+        print('---' * len(fila))
+
+def obtener_movimientos_posibles(tablero, posicion):
+    # No salir de los limites del tablero
+
+    fila, col = posicion
+    alto = len(tablero)
+    ancho = len(tablero[0])
+    movimientos_validos = []
+
+    # (-1, 0) -> Arriba
+    # ( 1, 0) -> Abajo
+    # ( 0,-1) -> Izquierda
+    # ( 0, 1) -> Derecha
+
+    direcciones = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    for df, dc in direcciones:
+        nueva_fila, nueva_col = fila + df, col + dc
+
+        if 0 <= nueva_fila < alto and 0 <= nueva_col < ancho:
+            movimientos_validos.append((nueva_fila, nueva_col))
+
+    return movimientos_validos
+
+# -------------------------------------------------------------------------------
+# COMO SE EJECUTA EL JUEGO
+
+ANCHO_TABLERO = 8
+ALTO_TABLERO = 8
+
+#El gato empieza en la esquina superior izquierda 
+posicion_inicial_gato = (0, 0)
+
+#El raton empieza en la esquina inferior derecha
+posicion_inicial_raton = (7, 7)
+
+NUMERO_DE_TURNOS = 5 #Duracion de la simulacion por turnos
+
+#EJECUCION DE LOGICA
+print("¡Bienvenido al laberinto del Gato y el Raton!")
+
+mi_tablero = crear_tablero(ANCHO_TABLERO, ALTO_TABLERO)
+
+posicion_actual_gato = posicion_inicial_gato
+posicion_actual_raton = posicion_inicial_raton
+
+colocar_personajes(mi_tablero, posicion_inicial_gato, posicion_inicial_raton)
+
+print("--- Turno 0: Estado Inicial ---")
+imprimir_tablero(mi_tablero)
+
+#Bucle principal del juego
+
+for turno in range(1, NUMERO_DE_TURNOS + 1):
+    print(f"\n--- Turno {turno} ---")
+
+    #MUEVE EL RATON
+    movimientos_raton = obtener_movimientos_posibles(mi_tablero, posicion_actual_raton)
+
+    if movimientos_raton: #SI la lista de movimientos esta vacia
+        #Se elige movimiento valido al azar 
+        nueva_posicion_raton = random.choice(movimientos_raton)
+
+        #Se actualiza el tablero y se borra la vieja ubicacion del raton
+        fila_vieja, col_vieja = posicion_actual_raton
+        mi_tablero[fila_vieja][col_vieja] = VACIO
+
+        #Se le coloca a su nueva ubicacion
+        fila_nueva, col_nueva = nueva_posicion_raton
+        mi_tablero[fila_nueva][col_nueva] = RATON
+
+        posicion_actual_raton = nueva_posicion_raton
+        print(f"El ratón se mueve a {posicion_actual_raton}")
+    else: 
+        print("El ratón no tiene dónde moverse. ¡Está atrapado!")
+    
+    imprimir_tablero(mi_tablero)
+
